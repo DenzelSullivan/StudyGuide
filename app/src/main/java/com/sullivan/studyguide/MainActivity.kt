93080cc72d2b5e20589ceb420cad3a8eb444a4ea
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-04-23 20:06:39 -0400

    Added simple notification

--- app/src/main/java/com/sullivan/studyguide/MainActivity.kt
+++ app/src/main/java/com/sullivan/studyguide/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.sullivan.studyguide
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        createChannel(getString(R.string.channel_id), "Study Guide Notification")
+
         toastButton.setOnClickListener {
             Toast.makeText(this, getString(R.string.toast_text), Toast.LENGTH_LONG).show()
         }
@@ -19,5 +26,38 @@ class MainActivity : AppCompatActivity() {
         snackButton.setOnClickListener {
             Snackbar.make(it, getString(R.string.snack_text), Snackbar.LENGTH_LONG).show()
         }
+
+        notificationButton.setOnClickListener {
+            val notificationManager = ContextCompat.getSystemService(
+                this@MainActivity.applicationContext,
+                NotificationManager::class.java
+            ) as NotificationManager
+
+            notificationManager.sendNotification(
+                "Time to Study!!",
+                this@MainActivity.applicationContext
+            )
+        }
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        val notificationChannel = NotificationChannel(
+            channelId,
+            channelName,
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            setShowBadge(false)
+        }
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor = Color.RED
+        notificationChannel.enableVibration(true)
+        notificationChannel.description = "Base Notification"
+
+        // for fragment
+        /*val notificationManager = requireActivity().getSystemService(
+            NotificationManager::class.java
+        )*/
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(notificationChannel)
     }
 }
